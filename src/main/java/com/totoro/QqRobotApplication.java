0fd5aa0d1e51042cc1c053f545a190ff18deb613
totoro <1026215903@qq.com>
2023-03-16 17:27:13 +0800

    feat: QQbot

--- src/main/java/com/totoro/QqRobotApplication.java
+++ src/main/java/com/totoro/QqRobotApplication.java
@@ -1,8 +1,10 @@
 package com.totoro;
 
+import love.forte.simboot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSimbot
 @SpringBootApplication
 public class QqRobotApplication {
 
---
+++ src/main/java/com/totoro/listener/MyListener.java
@@ -0,0 +1,13 @@
+package com.totoro.listener;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author:totoro
+ * @createDate:2023/3/16
+ * @description:
+ */
+@Component
+public class MyListener {
+
+}
